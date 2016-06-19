0b6c86e542a2ec9dfd1a661af046a0980b2cf564
pierrdu <pierre@duhem.com>
2016-06-19 08:41:16 +0200

    1.0.1

--- language/en/alphasort.php
+++ language/en/alphasort.php
@@ -19,50 +19,22 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, array(
 
 	// Menu text and titles
-	'ACP_AUTOLINKS_TITLE'			=> 'Autolink Administration',
-	'AUTOLINK_ADD_A_NEW_WORD'		=> 'Add new Autolink',
-	'RECURS_FEATURE'				=> 'Prevent recursive replacement',
-	'RECURS_FEATURE_EXPLAIN'			=> 'If the URLs contain the terms to be replaced, there is a risk of recursive replacement and the links don\'t work as they should. This option prevents the recursive replacement but a side effect is that the term gets replaced by the exact form entered in the table below.',
-	'TERM_ADDITION'				=> 'Adding a new term',
-	'TERM_ADDITION_EXPLAIN'			=> 'This options displays a form to create a new term.',
+	'AUTOLINK_MOD_MENU_NAME'			=> 'Extension Alphasort',
+	'ACP_ALPHASORT_TITLE'			=> 'Alphabetical sort of topics',
 	'PST_LEGEND3'					=> 'Forum settings',
-	'PST_NOSHOW_LIST'				=> 'Enable autolinks in',
+	'PST_NOSHOW_LIST'				=> 'Enable sort',
 	'LEGEND_ADD_TERM'				=> 'Term Management',
 	'ACP_CHECKALL'					=> 'Select/deselect',
 	'ACP_CHECKALL_EXPLAIN'			=> 'Here you can check/uncheck all forums together.',
 	'ACP_ALL_FORUMS'				=> 'All forums',
 
+	
+	// Main form's words
+	'ACP_ALPHASORT_CONFIG'			=> 'Extension configuration',
+
 	'ALPHASORT' 					=> 'Sort topics',
-	'ALL_TOPICS' 					=> 'All topics',
+	'ALL_TOPICS' 					=> 'No sorting',
 	'SORT_SYMBOLS' 				=> 'Symbols',
 
-	// Main form's words
-	'ACP_AUTOLINKS_CONFIG'			=> 'Autolink configuration',
-
-	'ACP_AUTOLINK_WORDS'			=> 'Manage words',
-	'ACP_AUTOLINK_WORD'				=> 'Word',
-	'ACP_AUTOLINK_WORD_NOTE'			=> 'Here you can set the word to convert to a link. The Autolink extension is not case sensitive.',
-	'ACP_AUTOLINK_URL'				=> 'URL',
-	'ACP_AUTOLINK_URL_NOTE'			=> 'Here you can set one URL for the word above.',
-
-	// Logs
-	'LOG_AUTOLINK_WORD_ADDED'		=> 'A new word (%s) was successfully added to the Autolink database!',
-	'LOG_AUTOLINK_WORD_EDIT'			=> 'The details of “%s” word was updated in the Autolink database!',
-	'LOG_AUTOLIMK_WORD_DELETE'		=> '“%s” word was removed from the Autolink database!',
-	'LOG_AUTOLINK_CONFIG_UPDATED'		=> 'The Autolink config settings were updated.',
-
-	// ACP table heading words
-	'AUTOLINK_NAME'				=> 'Word',
-	'AUTOLINK_URL'					=> 'URL',
-
-	// Error messages
-	'AUTOLINK_NOT_ADDED'			=> 'The word wasn’t added to the Autolink database!',
-	'AUTOLINK_NOT_REMOVED'			=> 'The word wasn’t removed from the Autolink database!',
-	'AUTOLINK_NOT_UPDATED'			=> 'The word wasn’t updated in the Autolink database!',
-	'AUTOLINK_INVALID_ID'			=> 'You try to edit or remove a word with a wrong ID!',
-	'AUTOLINK_DIFFERENT_SIZE_ARRAY'	=> 'The amount of the added URLs and the rates have to be equall!',
-	'INVALID_FORM_KEY'				=> 'The form’s token key is invalid!',
-	'AUTOLINK_EMPTY_WORD_FIELD'		=> 'You have to fill the word input field!',
-	'AUTOLINK_EMPTY_URL_FIELD'		=> 'You have to fill the URL input field!',
-	'AUTOLINK_WORD_ALREADY_EXIST'		=> 'This word already exists!')
+	)
 );
--- language/fr/alphasort.php
+++ language/fr/alphasort.php
@@ -20,32 +20,17 @@ $lang = array_merge($lang, array(
 	// Menu text and titles
 	'AUTOLINK_MOD_MENU_NAME'			=> 'Extension Alphasort',
 	'ACP_ALPHASORT_TITLE'			=> 'Tri alphabétique des sujets',
-	'AUTOLINK_ADD_A_NEW_WORD'		=> 'Ajouter un nouveau terme',
-	'RECURS_FEATURE'				=> 'Empêcher le remplacement récursif',
-	'RECURS_FEATURE_EXPLAIN'			=> 'Si les URL saisies contiennent l\'un des termes servant de mot-clef, il existe un risque de remplacement récursif. Cette option empêche le remplacement récursif, mais a pour effet que le terme est remplacé par le terme exact saisi dans la table ci-dessous.',
-	'TERM_ADDITION'				=> 'Addition d\'un nouveau terme',
-	'TERM_ADDITION_EXPLAIN'			=> 'Appel d\'un formulaire de création d\'un nouveau terme.',
 	'PST_LEGEND3'					=> 'Paramétrage des forums',
-	'PST_NOSHOW_LIST'				=> 'Autoriser le tri dans',
+	'PST_NOSHOW_LIST'				=> 'Autoriser le tri',
 	'ACP_CHECKALL'					=> 'Sélection/désélection',
 	'ACP_CHECKALL_EXPLAIN'			=> 'Vous pouvez ici sélectionner/déselectionner tous les forums en une seule fois.',
 	'ACP_ALL_FORUMS'				=> 'Tous les forums',
 
 	// Main form's words
 	'ACP_ALPHASORT_CONFIG'			=> 'Configuration de l\'extension',
-
 	'ALPHASORT' 					=> 'Tri alphabétique',
-	'ALL_TOPICS' 					=> 'Tous les sujets',
+	'ALL_TOPICS' 					=> 'Pas de tri',
 	'SORT_SYMBOLS' 				=> 'Symboles',
 
-	// Error messages
-	'AUTOLINK_NOT_ADDED'			=> 'Le terme n\'a pas été ajouté dans la table.',
-	'AUTOLINK_NOT_REMOVED'			=> 'Le terme n\'a pas été supprimé dans la table.',
-	'AUTOLINK_NOT_UPDATED'			=> 'Le terme n\'a pas été mis à jour dans la table.',
-	'AUTOLINK_INVALID_ID'			=> 'Le numéro du terme que vous voulez éditer ou supprimer n\'existe pas.',
-	'AUTOLINK_DIFFERENT_SIZE_ARRAY'	=> 'Le nombre des valeurs de fréquence et des URL doit être identique.',
-	'AUTOLINK_EMPTY_WORD_FIELD'		=> 'La zone de saisie du terme est vide.',
-	'AUTOLINK_EMPTY_URL_FIELD'		=> 'La zone de saisie de l\'URL est vide.',
-	'AUTOLINK_WORD_ALREADY_EXIST'		=> 'Ce terme existe déjà.',
 	)
 );
