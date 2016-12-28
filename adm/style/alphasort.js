57b2cf751d815d9176e1ad9f3c0cdafb52b95b04
pierrdu <pierre@duhem.com>
2016-12-28 11:19:52 +0100

    1.1.5b

--- adm/style/alphasort.js
+++
@@ -1,7 +0,0 @@
-function alphasort_check_all(source) {
-	var checkboxes = document.getElementsByName('mark_alphasort_forum[]');
-	var n=checkboxes.length;
-	for(var i=0; i<n; i++) {
-		checkboxes[i].checked = source.checked;
-	}
-}
